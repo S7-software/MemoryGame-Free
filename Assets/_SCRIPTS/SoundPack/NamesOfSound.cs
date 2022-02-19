81861fbb8de0ba3df0ab0e53aa44a1b42d3d7ac6
Sinan TEKİN <37111233+sinan73k1n@users.noreply.github.com>
2022-02-19 17:32:14 +0200

    Yeni klasör ekleme ve ses düzenlemesi

---
+++ Assets/_SCRIPTS/SoundPack/NamesOfSound.cs
@@ -0,0 +1,10 @@
+public enum NamesOfSound
+{
+    bayrakKaldir, bayrakKoy,
+    clickArama, clickCikis, clickGiris,
+    KazandiSucsses, KazandiTada,
+    patlama,
+    bolumSecme, bolumTamamlandi1,bolumTamamlandi2, bolumTamamlandi0,
+    click, clickGeri0, clickGeri1,
+    kartBulma, kartSecme, oyunBasladi
+}
